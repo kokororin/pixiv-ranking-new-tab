b1774d7b4478e9dd46c82286c1edecf14ee35d06
Kokororin <i@kotori.love>
2016-11-29 12:22:41 +0800

    rectify && avoid 304 cache when new ranking comes at 11:30 JST

--- app.js
+++ app.js
@@ -19,165 +19,260 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
-var App = (function() {
-  'use strict';
-  return {
-    data: null,
-    index: 0,
+'use strict';
+var App = React.createClass({
+  displayName: 'AppComponent',
+
+  config: {
     interValTime: 6500,
     tokyoTimeZone: 'Asia/Tokyo',
+    menuItems: [{
+      i18nString: 'history',
+      url: 'chrome://history'
+    }, {
+      i18nString: 'bookmarks',
+      url: 'chrome://bookmarks'
+    }, {
+      i18nString: 'apps',
+      url: 'chrome://apps'
+    }]
+  },
 
-    run: function() {
-      this.renderChromeLink();
-      this.render();
-    },
+  getInitialState: function() {
+    return {
+      response: null,
+      index: 0,
+      item: {}
+    };
+  },
 
-    renderChromeLink: function() {
-      for (var i = 0, l = document.querySelectorAll('.chrome-link').length; i < l; i++) {
-        var element = document.querySelectorAll('.chrome-link')[i];
-        element.innerHTML = chrome.i18n.getMessage(element.dataset.name);
-        element.addEventListener('click', function(event) {
-          event.preventDefault();
-          chrome.tabs.update({
-            url: this.dataset.url
-          });
-        });
-      }
-    },
+  componentDidMount: function() {
+    var data = localStorage.getItem('ranking');
+    var cachedDate = localStorage.getItem('ranking:date');
+    var tokyoDate = moment().tz(this.config.tokyoTimeZone).format('YYYY-MM-DD'); // 2016-11-28
+    var updateDate = tokyoDate + 'T12:30:00+09:00'; // 2016-11-28T12:30:00+09:00
+    updateDate = moment(updateDate).tz(this.config.tokyoTimeZone);
+    var nowDate = moment().tz(this.config.tokyoTimeZone);
+    var shouldUpdate = false;
 
-    render: function() {
-      this.data = localStorage.getItem('ranking');
-      var cachedDate = localStorage.getItem('ranking:date');
-      var tokyoDate = moment().tz(this.tokyoTimeZone).format('YYYY-MM-DD'); // 2016-11-28
-      var updateDate = tokyoDate + 'T12:30:00+09:00'; // 2016-11-28T12:30:00+09:00
-      updateDate = moment(updateDate).tz(this.tokyoTimeZone);
-      var nowDate = moment().tz(this.tokyoTimeZone);
-
-      var shouldUpdate = false;
-      if (this.data === null && cachedDate === null) {
+    if (data === null && cachedDate === null) {
+      shouldUpdate = true;
+    } else {
+      if (cachedDate === moment(tokyoDate, 'YYYY-MM-DD').add('-1', 'days').format('YYYY-MM-DD')) {
+        shouldUpdate = false;
+      } else if (nowDate.isAfter(updateDate)) {
         shouldUpdate = true;
       } else {
-        if (cachedDate === moment(tokyoDate, 'YYYY-MM-DD').add('-1', 'days').format('YYYY-MM-DD')) {
-          shouldUpdate = false;
-        } else if (nowDate.isAfter(updateDate)) {
-          shouldUpdate = true;
-        } else {
-          shouldUpdate = false;
-        }
+        shouldUpdate = false;
       }
+    }
 
-      if (shouldUpdate) {
-        var request = new XMLHttpRequest();
-        request.open('GET', 'https://api.pixiv.moe/v1/ranking', true);
-        request.onload = function() {
-          this.processResponse(request);
-          this.showNotification({
-            title: chrome.i18n.getMessage('appName'),
-            message: chrome.i18n.getMessage('updated'),
-            iconUrl: 'logo-128.png'
-          });
-        }.bind(this);
-        request.onerror = this.showError;
-        request.send();
-      } else {
-        var cachedRequest = {
-          status: 200,
-          responseText: this.data
-        };
-        this.processResponse(cachedRequest);
-      }
-    },
+    if (shouldUpdate) {
+      var request = new XMLHttpRequest();
+      request.open('GET', 'https://api.pixiv.moe/v1/ranking', true);
+      request.onload = function() {
+        this.processResponse(request);
+        this.showNotification({
+          title: chrome.i18n.getMessage('appName'),
+          message: chrome.i18n.getMessage('updated'),
+          iconUrl: 'logo-128.png'
+        });
+      }.bind(this);
+      request.onerror = this.setError;
+      request.send();
+    } else {
+      var cachedRequest = {
+        status: 200,
+        responseText: data
+      };
+      this.processResponse(cachedRequest);
+    }
+  },
 
-    cutString: function(str, len) {
-      if (str.length * 2 <= len) {
-        return str;
-      }
-      var strlen = 0;
-      var s = "";
-      for (var i = 0; i < str.length; i++) {
-        s = s + str.charAt(i);
-        if (str.charCodeAt(i) > 128) {
-          strlen = strlen + 2;
-          if (strlen >= len) {
-            return s.substring(0, s.length - 1) + "...";
-          }
-        } else {
-          strlen = strlen + 1;
-          if (strlen >= len) {
-            return s.substring(0, s.length - 2) + "...";
-          }
-        }
-      }
-      return s;
-    },
+  processResponse: function(o) {
+    if (o.status >= 200 && o.status < 400) {
+      var data = JSON.parse(o.responseText);
 
-    carousel: function() {
-      var works = this.data.response.works;
-      var val = works[this.index];
-      document.querySelector('body').style.backgroundImage = 'url(https://api.pixiv.moe/v1/ranking/' + val.rank + ')';
-      var footerWidth = document.querySelector('.footer').offsetWidth;
-      var rankWidth = document.querySelector('.footer .rank').offsetWidth;
-      var cutLength = Math.ceil((Math.ceil((footerWidth - rankWidth) / 40) * 1.3));
-      document.querySelector('.footer .title').innerHTML = '<a href="http://www.pixiv.net/i/' + val.work.id + '">' + this.cutString(val.work.title, cutLength) + '</a>';
-      var rankHTML = val.rank + '位';
-      rankHTML += '<div class="yesterday">';
-      if (val.previous_rank === 0) {
-        rankHTML += '初登場';
-      } else if (val.previous_rank > val.rank) {
-        rankHTML += '<span class="compare">↑</span> ' + '前日 ' + val.previous_rank + '位';
+      if (data.status === 'success') {
+        this.setState({
+          response: data.response
+        }, function() {
+          this.carousel();
+          setInterval(this.carousel.bind(this), this.config.interValTime);
+        });
+        localStorage.setItem('ranking', o.responseText);
+        localStorage.setItem('ranking:date', data.response.date);
+      }
+    } else {
+      this.setError();
+    }
+  },
+
+  carousel: function() {
+    var works = this.state.response.works;
+    var val = works[this.state.index];
+    var cachedDate = localStorage.getItem('ranking:date');
+    cachedDate = moment(cachedDate, 'YYYY-MM-DD').format('YYYYMMDD');
+    // avoid 304 cache
+
+    document.body.style.backgroundImage = 'url(https://api.pixiv.moe/v1/ranking/' + val.rank + '?v=' + cachedDate + ')';
+    var footerWidth = this.footerRef.offsetWidth;
+    var rankWidth = this.rankRef.offsetWidth;
+    var cutLength = Math.ceil((Math.ceil((footerWidth - rankWidth) / 40) * 1.3));
+    this.setItem('title', this.cutString(val.work.title, cutLength));
+    this.setItem('url', 'http://www.pixiv.net/i/' + val.work.id);
+    this.setItem('rankNum', val.rank + '位');
+    var icon;
+    if (val.previous_rank === 0) {
+      this.setItem('rankMetaText', '初登場');
+      this.setItem('rankMetaIcon', null);
+    } else {
+      this.setItem('rankMetaText', '前日 ' + val.previous_rank + '位');
+      if (val.previous_rank > val.rank) {
+        icon = '↑';
       } else if (val.previous_rank < val.rank) {
-        rankHTML += '<span class="compare">↓</span> ' + '前日 ' + val.previous_rank + '位';
+        icon = '↓';
       }
-      rankHTML += '</div>';
-      document.querySelector('.footer .rank').innerHTML = rankHTML
-      this.index++;
-      if (this.index >= works.length) {
-        this.index = 0;
-      }
-    },
+      this.setItem('rankMetaIcon', React.createElement('span', {
+        className: 'compare'
+      }, icon));
+    }
 
-    showError: function() {
-      document.querySelector('.footer .title').innerHTML = '<a href="#" id="reload">読み込みに失敗しました</a>';
-      document.querySelector('#reload').addEventListener('click', function(event) {
-        event.preventDefault();
+    this.setState({
+      index: this.state.index >= (works.length - 1) ? 0 : (this.state.index + 1)
+    });
+  },
+
+  onChromeLinkClick: function(event) {
+    var target = event.nativeEvent.target;
+    chrome.tabs.update({
+      url: target.dataset.url
+    });
+  },
+
+  showNotification: function(opt, time) {
+    if (typeof time === 'undefined') {
+      time = 5000;
+    }
+    opt.type = opt.type || 'basic';
+    chrome.notifications.clear('notifyId');
+    var notification = chrome.notifications.create('notifyId', opt, function(notifyId) {
+      return notifyId;
+    });
+    setTimeout(function() {
+      chrome.notifications.clear('notifyId');
+    }, time);
+    return notification;
+  },
+
+  setItem: function(key, value) {
+    var item = this.state.item;
+    item[key] = value;
+    this.setState({
+      item: item
+    });
+  },
+
+  getItem: function(key) {
+    return this.state.item[key];
+  },
+
+  setError: function() {
+    this.setState({
+      isError: true
+    });
+  },
+
+  cutString: function(str, len) {
+    if (str.length * 2 <= len) {
+      return str;
+    }
+    var strlen = 0;
+    var s = "";
+    for (var i = 0; i < str.length; i++) {
+      s = s + str.charAt(i);
+      if (str.charCodeAt(i) > 128) {
+        strlen = strlen + 2;
+        if (strlen >= len) {
+          return s.substring(0, s.length - 1) + "...";
+        }
+      } else {
+        strlen = strlen + 1;
+        if (strlen >= len) {
+          return s.substring(0, s.length - 2) + "...";
+        }
+      }
+    }
+    return s;
+  },
+
+  renderTitleContent: function() {
+    if (this.state.isError) {
+      return this.renderError();
+    }
+    if (this.state.response === null) {
+      return null;
+    }
+    return React.createElement('a', {
+      href: this.getItem('url')
+    }, this.getItem('title'));
+  },
+
+  renderRankContent: function() {
+    if (this.state.response === null) {
+      return null;
+    }
+    return React.createElement('div', {
+      ref: function(ref) {
+        this.rankRef = ref;
+      }.bind(this),
+      className: 'rank'
+    }, this.getItem('rankNum'), React.createElement('div', {
+      className: 'yesterday'
+    }, this.getItem('rankMetaIcon'), this.getItem('rankMetaText')));
+  },
+
+  renderError: function() {
+    return React.createElement('a', {
+      href: '#',
+      onClick: function(event) {
+        event.nativeEvent.preventDefault();
         localStorage.removeItem('ranking');
         localStorage.removeItem('ranking:date');
         window.location.reload();
-      });
-    },
-
-    showNotification: function(opt, time) {
-      if (typeof time === 'undefined') {
-        time = 5000;
       }
-      opt.type = opt.type || 'basic';
-      chrome.notifications.clear('notifyId');
-      var notification = chrome.notifications.create('notifyId', opt, function(notifyId) {
-        return notifyId;
-      });
-      setTimeout(function() {
-        chrome.notifications.clear('notifyId');
-      }, time);
-      return notification;
-    },
+    }, '読み込みに失敗しました');
+  },
 
-    processResponse: function(o) {
-      if (o.status >= 200 && o.status < 400) {
-        this.data = JSON.parse(o.responseText);
+  render: function() {
+    return React.createElement('div', null, React.createElement('div', {
+      id: 'top-right',
+      className: 'right'
+    }, React.createElement('div', {
+      id: 'top-menu'
+    }, React.createElement('ul', {
+      className: 'nav navbar-nav navbar-right'
+    }, this.config.menuItems.map(function(elem) {
+      return React.createElement(
+        'li', null, React.createElement('a', {
+          'data-url': elem.url,
+          href: '#',
+          onClick: this.onChromeLinkClick
+        }, chrome.i18n.getMessage(elem.i18nString))
+      );
+    }.bind(this))))), React.createElement('footer', {
+      ref: function(ref) {
+        this.footerRef = ref;
+      }.bind(this),
+      className: 'footer'
+    }, React.createElement('div', {
+      className: 'title'
+    }, this.renderTitleContent()), this.renderRankContent())
+    );
+  }
 
-        if (this.data.status === 'success') {
-          this.carousel();
-          setInterval(this.carousel.bind(this), this.interValTime);
-          localStorage.setItem('ranking', o.responseText);
-          localStorage.setItem('ranking:date', this.data.response.date);
-        }
-      } else {
-        this.showError();
-      }
-    }
-  };
-})();
+});
 
 document.addEventListener('DOMContentLoaded', function() {
-  App.run();
+  ReactDOM.render(React.createElement(App), document.querySelector('#app'));
 });
